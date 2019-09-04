79ae4e9f2408ecaa683bac17dfe83a545190e288
Gwendal <gwendal.daniel@gmail.com>
2019-09-04 19:23:11 +0200

    Use new webhook/handler (refs xatkit-bot-platform/xatkit-runtime/issues/219)

--- runtime/src/main/java/com/xatkit/plugins/wordpress/platform/io/RestProvider.java
+++ runtime/src/main/java/com/xatkit/plugins/wordpress/platform/io/RestProvider.java
@@ -1,18 +1,20 @@
 package com.xatkit.plugins.wordpress.platform.io;
 
-import com.google.gson.JsonElement;
 import com.xatkit.core.platform.io.EventInstanceBuilder;
 import com.xatkit.core.platform.io.JsonEventMatcher;
-import com.xatkit.core.platform.io.JsonWebhookEventProvider;
+import com.xatkit.core.platform.io.WebhookEventProvider;
+import com.xatkit.core.server.JsonRestHandler;
+import com.xatkit.core.server.RestHandlerFactory;
 import com.xatkit.core.session.XatkitSession;
 import com.xatkit.intent.EventInstance;
 import com.xatkit.plugins.wordpress.platform.WordPressPlatform;
 import org.apache.commons.configuration2.Configuration;
-import org.apache.http.Header;
 
-public class RestProvider extends JsonWebhookEventProvider<WordPressPlatform> {
+public class RestProvider extends WebhookEventProvider<WordPressPlatform, JsonRestHandler> {
 
     private final static String WP_EVENT_HEADER_KEY = "WordPress-Event";
+
+    private final static String ENDPOINT_URI = "/wordpress";
 
     private JsonEventMatcher matcher;
 
@@ -25,20 +27,18 @@ public class RestProvider extends JsonWebhookEventProvider<WordPressPlatform> {
     }
 
     @Override
-    protected void handleParsedContent(JsonElement jsonElement, Header[] headers) {
-        EventInstance eventInstance = matcher.match(headers, jsonElement);
-        XatkitSession xatkitSession = xatkitCore.getOrCreateXatkitSession("wordpress");
-        this.sendEventInstance(eventInstance, xatkitSession);
+    public String getEndpointURI() {
+        return ENDPOINT_URI;
     }
 
     @Override
-    public void run() {
-        synchronized (this) {
-            try {
-                wait();
-            } catch(InterruptedException e) {
-
-            }
-        }
+    protected JsonRestHandler createRestHandler() {
+        return RestHandlerFactory.createJsonRestHandler((headers, params, content) -> {
+            EventInstance eventInstance = matcher.match(headers, content);
+            XatkitSession xatkitSession = xatkitCore.getOrCreateXatkitSession("wordpress");
+            this.sendEventInstance(eventInstance, xatkitSession);
+            return null;
+        });
     }
+
 }
